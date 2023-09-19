d2340b4a2f577a7f4d699c1941dc2dcdbe09c553
Viktor Dojchinovski <viktor@Viktors-MacBook-Pro.local>
2023-09-20 00:01:31 +0200

    Finish basic start

---
+++ BasicSyntax.js
@@ -0,0 +1,31 @@
+/**
+    The following content explains the basic syntax rules 
+    that has to be followed in the JavaScript programming language.
+
+    They are no-op rules, which means that we have to abide by them.
+    Still, starting JavaScript is not that hard after all.
+    So, here are some key points:
+
+    1. Statements end with a semicolon (;).​​
+
+    2. Code blocks are enclosed in curly braces ({ }).​​
+
+    3. JavaScript is case-sensitive (e.g., myVariable is different from myvariable).​​
+
+    4. Comments can be single-line (//) or multi-line (\/* *\/).​
+*/
+
+/**
+ * Functions
+ *** reusable blocks of code that can be defined once and executed (invoked) multiple times. ***
+ */
+
+/**
+ * Loops
+ *** structures that allow you to repeat a block of code multiple times until a certain condition is met. ***
+ */
+
+/**
+ * Conditionals
+ *** control structures that allow you to make decisions in your code based on specified conditions. ***
+ */
